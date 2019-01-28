5da58e5468f49760bfa04e3e7c983bbc09c19c03
js103 <643929860@qq.com>
2019-01-28 22:39:35 +0700

    edit格式化 html插件引入

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
-let path = require("path")
+let path = require("path");
+let HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    devServer: {
-        // 开发服务器配置 webpack-dev-server
-        // 起服务构建的时候需要html文件，否则不会显示页面，只会显示路径
-        port: 3000,
-        progress: true,
-        contentBase: './build'
-    },
+	devServer: {
+		// 开发服务器配置 webpack-dev-server
+		// 起服务构建的时候需要html文件，否则不会显示页面，只会显示路径
+		port: 3000,
+		progress: true,
+		contentBase: "./build"
+	},
 
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "bundle.js", //打包后的文件名
-        path: path.resolve(__dirname, "./build") // 路径必须是一个绝对路径
-    }
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js", //打包后的文件名
+		path: path.resolve(__dirname, "./build") // 路径必须是一个绝对路径
+	},
+
+	plugins: [
+		// 所有webpack 插件
+		new HtmlWebpackPlugin({
+			template: "./src/index.html", //模板
+			filename: "index.html", //文件名
+			hash: true,
+			minify: {
+				// 压缩
+				removeAttributeQuotes: true, //删除属性双引号
+				collapseWhitespace: true //折叠为一行
+			}
+		})
+	]
 };
-
