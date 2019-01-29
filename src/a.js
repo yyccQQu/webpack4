fa39a63a08742fcff4eaa7c7d6c7938253a708a6
js103 <643929860@qq.com>
2019-01-29 16:07:24 +0700

    es7及以上 require('@babel/polyfill')

--- src/a.js
+++ src/a.js
@@ -1,9 +1,16 @@
+import { AST_Await } from "terser";
+require('@babel/polyfill')
+
 console.log("a.js")
 class B{
     
 }
 
+// "@babel/runtime" -S
 function * gen(params) {
     yield 1;
 }
 console.log(gen().next())
+
+//babel/polyfill S
+'aaa'.includes('a');
