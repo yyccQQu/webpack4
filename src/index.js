e4cbd2dea3fc5611dd53db45540fa2fc86bccf53
js103 <643929860@qq.com>
2019-01-29 16:49:21 +0700

    init jq

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 // import $ from 'expose-loader?$!jquery'
 // expose-loader 暴露 全局的loader 内联的loader
-import $ from "jquery";
+// import $ from "jquery";
 // console.log($,'///',window.$)
 
 //直接取 jquery
