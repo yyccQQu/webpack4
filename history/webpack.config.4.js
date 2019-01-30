d6bbdb023a242eacf6b19df510ff72ed5f1c2643
js103 <643929860@qq.com>
2019-01-30 13:00:41 +0700

    环境变量

--- history/webpack.config.4.js
+++ history/webpack.config.4.js
@@ -1,6 +1,5 @@
 let path = require('path')
 let HtmlWebpackPlugin = require('html-webpack-plugin')
-let webpack = require('webpack')
 
 module.exports = {
 	mode: "production",
@@ -38,16 +37,29 @@ module.exports = {
             bootstrapCss: "bootstrap/dist/css/bootstrap.css",
 		}
 	},
-	
+	devServer: {
+		// 3) 有服务端 不用代理处理,
+		//    前端和服务端启动在一个端口上
+		//    在服务端启动webpack， 端口用服务端端口
+		// 2）单纯模拟数据
+		// before(app) {
+		//     app.get("/api/user", (req, res) => {
+		// 		res.json({ name: "yyccqqu1" });
+		// 	});
+		// }
+		//
+		// 1）
+		// proxy: {
+		// 	"/api": {
+		// 		target: "http://localhost:3000",
+		// 		pathRewrite: {'/api':''}
+		// 	}
+		// }
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./index.html",
 			filename: "index.html" //但文件的时候改为index，要不找不到
-        }),
-        new webpack.DefinePlugin({
-            DEV: JSON.stringify('production'),
-            FLAG: 'true',
-            EXPRESSION: '1+1'
-        })
+		})
 	]
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 
-// import "bootstrapCss";
-import 'bootstrap';
-import "./style";
+let url = "";
 
+if(DEV === 'dev'){
+    url = "http://localhost:3000";
+}else{
+    url = 'http://www.baidu.com'
+} 
+
+console.log(url,'------')
+
+console.log(typeof FLAG)
+console.log(EXPRESSION)
