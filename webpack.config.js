c2d0e6fde5a705e09ca872801529655406be08ad
js103 <643929860@qq.com>
2019-01-30 16:35:01 +0700

    多线程 happypack使用

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ let path = require('path')
 let HtmlWebpackPlugin = require('html-webpack-plugin')
 let CleanWebpackPlugin = require("clean-webpack-plugin");
 let webpack = require('webpack')
+const HappyPack = require("happypack");
 
 
 module.exports = {
@@ -10,42 +11,62 @@ module.exports = {
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist")
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        contentBase: './dist'
-    },
-    module:{
-        noParse:/jquery/, //不去解析jquery中的依赖关系
-        rules:[
-            {
-                test:/\.js$/,
-                exclude: /node_modules/,
-                include: path.resolve('src'),
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets:[
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ]
-                    }
-                }
-            }
-        ]
-    },
+	},
+	devServer: {
+		port: 3000,
+		open: true,
+		contentBase: "./dist"
+	},
+	module: {
+		noParse: /jquery/, //不去解析jquery中的依赖关系
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				include: path.resolve("src"),
+				use: "happypack/loader?id=happybabel"
+				// use: {
+				// 	loader: "babel-loader",
+				// 	options: {
+				// 		presets: ["@babel/preset-env", "@babel/preset-react"]
+				// 	}
+				// }
+			},
+			{
+				test: /\.css$/,
+				use: "happypack/loader?id=happycss"
+			}
+		]
+	},
 	plugins: [
-        //引用 动态链接库（_dll_react.js）的插件
-        //寻找任务清单
-        // 如果找不到，再去打包那些import的文件
-        new webpack.DllReferencePlugin({
-            manifest: path.resolve(__dirname,'dist','mainfest.json')
-        }),
-        new webpack.IgnorePlugin(/\.\/locale/,/moment/),
+		//引用 动态链接库（_dll_react.js）的插件
+		//寻找任务清单
+		// 如果找不到，再去打包那些import的文件
+		new webpack.DllReferencePlugin({
+			manifest: path.resolve(__dirname, "dist", "mainfest.json")
+		}),
+		new webpack.IgnorePlugin(/\.\/locale/, /moment/),
 		new HtmlWebpackPlugin({
 			template: "./public/index.html"
 		}),
-		// new CleanWebpackPlugin("./dist")
+		// new CleanWebpackPlugin("./dist"),
+		new HappyPack({
+			id: "happybabel",
+			use: [
+				{
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}
+			]
+		}),
+		new HappyPack({
+            id: "happycss",
+            use: [
+                "style-loader",
+                "css-loader"
+            ]
+		})
 	]
 };
