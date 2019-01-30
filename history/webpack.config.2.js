db6b572a9957202e4ade4b22cd4ff3ec2bcbb0be
js103 <643929860@qq.com>
2019-01-30 09:26:05 +0700

    cleanWebpackPlugin--copyWebpackPlugin--bannerPlugin的使用

--- history/webpack.config.2.js
+++ history/webpack.config.2.js
@@ -1,16 +1,9 @@
 let path = require('path')
 let HtmlWebpackPlugin = require('html-webpack-plugin')
-let CleanWebpackPlugin = require('clean-webpack-plugin')
-let CopyWebpackPlugin = require('copy-webpack-plugin')
-let webpack = require('webpack')
-
-// 小插件
-// 1) cleanWebpackPlugin
-// 2) copyWebpackPlugin
-// 3) bannerPlugin 内置  版权申明插件
 
 module.exports = {
 	mode: "production",
+	// 多入口
 	entry: {
 		home: "./src/index.js"
 	},
@@ -31,15 +24,16 @@ module.exports = {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "dist")
 	},
+    watch: true,
+    watchOptions: {
+        poll: 1000, // 每秒监控
+        aggregateTimeout: 500, //防抖 ，0.5秒内有变化就延时编译
+        ignored: /node_modules/ //忽略文件
+    },
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./index.html",
 			filename: "index.html" //但文件的时候改为index，要不找不到
-		}),
-		new CleanWebpackPlugin("./dist"),
-		new CopyWebpackPlugin([
-            { from:"doc", to:"./"} //将doc目录下内容拷贝到dist目录下
-        ]),
-        new webpack.BannerPlugin('make 2019 by yyccqqu')
+		})
 	]
 };
