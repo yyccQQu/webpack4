9c99b36b32bc5552b4df963832540512107ffaa6
js103 <643929860@qq.com>
2019-01-31 22:23:30 +0700

    AST 递归解析 ---》 webpack打包格式重现

--- yc-pack/lib/Compiler.js
+++ yc-pack/lib/Compiler.js
@@ -1,6 +1,15 @@
 let fs = require('fs')
 let path = require('path')
 let processs = require('process')
+let babylon = require('babylon')
+let t = require('@babel/types')
+let traverse = require("@babel/traverse").default;
+let generator = require('@babel/generator').default
+
+// babylon 主要就是把源码 转换成ast
+// @babel/traverse
+// @babel/types 
+// @babel/generator
 
 
 class Compiler {
@@ -19,7 +28,25 @@ class Compiler {
     }
     // 解析源码
     parse(source, parentPath) { //AST解析语法树
-        console.log(source, parentPath,'ppparentpath');
+        //https://astexplorer.net/ 
+        //左侧栏中输入require("./a"),看右边的语法解析
+        let ast = babylon.parse(source); 
+        let dependencies = []; //依赖的数组
+        traverse(ast,{
+            CallExpression(p) { //调用表达式: a() b()
+                let node = p.node; //AST语法 对应的节点
+                if(node.callee.name === 'require'){
+                    node.callee.name = "__webpack_require__";
+                    let moduleName = node.arguments[0].value; //取到模块的引用名字
+                    moduleName = moduleName + (path.extname(moduleName)?"":".js");//
+                    moduleName ='./'+ path.join(parentPath, moduleName); //'src/a.js'
+                    dependencies.push(moduleName); //将路径放入(依赖的数组)
+                    node.arguments = [t.stringLiteral(moduleName)];
+                }
+            }
+        });
+        let sourceCode = generator(ast).code
+        return {sourceCode, dependencies}
     }
 	// 构建模块
 	buildModule(modulePath, isEntry) {
@@ -29,23 +56,37 @@ class Compiler {
         // let moduleName = this.entry
         let moduleName = './'+path.relative(this.root,modulePath);
        
-        if(isEntry){
+        if(isEntry){ //为true就为父模块
             this.entryId = moduleName; //保存入口的名字
         }
-        console.log(source, '123', moduleName),
-        console.log(this.root, this.entry, modulePath);
+        console.log("source:--->", source);
+        console.log("moduleName:--->", moduleName);
+        console.log("this.root:--->", this.root)
+        console.log("this.entry:--->", this.entry)
+        
         //解析需要把source源码进行改造， 返回一个依赖列表
         let{sourceCode,dependencies} = this.parse(source, path.dirname(moduleName))
+        // console.log(sourceCode,dependencies,'sdependCode')
+        console.log("sourceCode:--->", sourceCode)
+        console.log("dependencies:--->", dependencies)
+        
         //把相对路径和模块中的内容 对应起来
         this.modules[moduleName] = sourceCode
+
+        dependencies.forEach(dep => { //附属模块的递归加载
+            this.buildModule(path.join(this.root,dep),false);//后面参数为false，表示为子模块
+        })
 	}
 	emitFile() {
-		//发射文件
+        //发射文件 
+        //用数据 渲染我们的打包结果
+        
+
 	}
 	run() {
 		//执行 并且创建模块的依赖关系
 		this.buildModule(path.resolve(this.root, this.entry), true);
-
+        console.log(this.modules,this.entryId)
 		//发射一个文件 打包后的文件
 		this.emitFile();
 	}
